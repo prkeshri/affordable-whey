4ee1fdb5dfc0499f16b5ef501c5fd4fffcc32aa1
Jérémie Zarca <jzarca01@users.noreply.github.com>
2018-10-14 18:14:06 +0200

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,8 +1,12 @@
 function ResultantServingsAndPriceForRequiredProteins(priceAsGivenInWebsite, servingsAsGivenInWebsite, proteinsPerServingAsGivenInWebsite, requiredProteins = 24){
-   const servingsAmount = (servingsAsGivenInWebsite * proteinsPerServingAsGivenInWebsite) / requiredProteins;
-   const pricePerServing = priceAsGivenInWebsite / servingsAmount;
+   const servingsAmount = (servingsAsGivenInWebsite * proteinsPerServingAsGivenInWebsite) / requiredProteins
+   const pricePerServing = priceAsGivenInWebsite / servingsAmount
    return {
 	   resultantServings: servingsAmount, 
 	   pricePerServing: pricePerServing
-   };
+   }
+}
+
+module.exports = {
+	ResultantServingsAndPriceForRequiredProteins
 }
